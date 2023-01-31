484d04df1d829840c85272952d91880a9335e90c
butterriolu <derpvin12@gmail.com>
2023-01-30 23:43:10 -0500

    capture solution details

--- response.js
+++ response.js
@@ -1,33 +1,60 @@
 const res = {
   status_code: 10,
-  lang: "javascript",
+  lang: "cpp",
   run_success: true,
-  status_runtime: "60 ms",
-  memory: 41888000,
-  code_answer: ["[0,1]"],
-  code_output: [],
-  std_output: ["", ""],
-  elapsed_time: 88,
-  task_finish_time: 1675135087093,
-  expected_status_code: 10,
-  expected_lang: "cpp",
-  expected_run_success: true,
-  expected_status_runtime: "0",
-  expected_memory: 6312000,
-  expected_code_answer: ["[0,1]"],
-  expected_code_output: [],
-  expected_std_output: ["", ""],
-  expected_elapsed_time: 10,
-  expected_task_finish_time: 1675133799804,
-  correct_answer: true,
-  compare_result: "1",
-  total_correct: 1,
-  total_testcases: 1,
-  runtime_percentile: null,
-  status_memory: "41.9 MB",
-  memory_percentile: null,
-  pretty_lang: "JavaScript",
-  submission_id: "runcode_1675135086.8690264_eE5DbTbSOa",
+  status_runtime: "8 ms",
+  memory: 10872000,
+  question_id: "1",
+  elapsed_time: 21,
+  compare_result: "111111111111111111111111111111111111111111111111111111111",
+  code_output: "",
+  std_output: "",
+  last_testcase: "",
+  expected_output: "",
+  task_finish_time: 1675138317367,
+  total_correct: 57,
+  total_testcases: 57,
+  runtime_percentile: 95.75340000000001,
+  status_memory: "10.9 MB",
+  memory_percentile: 41.14740000000002,
+  pretty_lang: "C++",
+  submission_id: "888514284",
   status_msg: "Accepted",
   state: "SUCCESS",
 };
+
+const err = {
+  status_code: 20,
+  lang: "cpp",
+  run_success: false,
+  compile_error: "Line 17: Char 3: error: unknown type name 's'",
+  full_compile_error:
+    "Line 17: Char 3: error: unknown type name 's'\n};s\n  ^\n1 error generated.",
+  status_runtime: "N/A",
+  memory: 0,
+  question_id: "1",
+  task_finish_time: 1675138603489,
+  elapsed_time: 0,
+  total_correct: null,
+  total_testcases: null,
+  runtime_percentile: null,
+  status_memory: "N/A",
+  memory_percentile: null,
+  pretty_lang: "C++",
+  submission_id: "888516351",
+  status_msg: "Compile Error",
+  state: "SUCCESS",
+};
+
+const solutionLanguage = res.lang;
+const solutionCode = res.expected_lang;
+
+const fetchUrl =
+  "https://leetcode.com/submissions/detail/runcode_1675136932.7746737_1vMCA8TmMf/check/";
+
+const endsWith_check_ = new RegExp("check/$");
+const contains_runcode_ = new RegExp("runcode");
+
+console.log(
+  endsWith_check_.test(fetchUrl) && !contains_runcode_.test(fetchUrl)
+);
--- scripts/injected.js
+++ scripts/injected.js
@@ -1,11 +1,67 @@
-// Original inspiration: override browser's XHR function
-// Source : https://stackoverflow.com/a/67390377
+// Original inspiration: override browser's XHR function.
+// Source: https://stackoverflow.com/a/67390377
 
 // Since the submission details are in fetch requests, intercept fetches instead.
 
-// parse the response and create a document for katsudon
+// Global variable - only one solution per page, updates on submission create.
+let solutionCode;
+
+const Utils = {
+  isCreateSolutionRequest: (fetchUrl) => {
+    // create submssion  /problems/*/submit/
+    // create testcase   /problems/*/interpret_solution/
+    const endsWith_submit_ = new RegExp("submit/$");
+
+    return endsWith_submit_.test(fetchUrl);
+  },
+  isCheckSolutionRequest: (fetchUrl) => {
+    const endsWith_check_ = new RegExp("check/$");
+    const contains_runcode_ = new RegExp("runcode");
+
+    return endsWith_check_.test(fetchUrl) && !contains_runcode_.test(fetchUrl);
+  },
+  toMegabytes: (bytes) => {
+    const MB = 1000000;
+    return bytes / MB;
+  },
+};
+
+// Parse the response and create a document for katsudon
+// Solution for Katsudon server is in the form of solution attributes
+/*
+  problem_id: number;
+  solution_language: string;
+  solution_code: string;
+  failed?: boolean;
+  error?: string;
+  runtime_ms?: number;
+  memory_usage_mb?: number;
+*/
 async function parseLeetcodeFetchResponse(response) {
-  console.log(response);
+  const isResolved = response.state === "SUCCESS";
+
+  if (!isResolved) return;
+
+  const problemId = parseInt(response.question_id);
+  const solutionPassed = response.run_success;
+  const solutionLanguage = response.pretty_lang;
+  const solutionError = solutionPassed ? response.status_msg : null;
+  const runtimeMS = solutionPassed
+    ? parseInt(response.status_runtime.split(" ")[0])
+    : null;
+  const memoryMB = solutionPassed ? Utils.toMegabytes(response.memory) : null;
+
+  const resolvedSubmission = {
+    problem_id: problemId,
+    solution_language: solutionLanguage,
+    solution_code: solutionCode,
+    failed: !solutionPassed,
+    error: solutionError,
+    runtime_ms: runtimeMS,
+    memory_usage_mb: memoryMB,
+  };
+
+  console.log(resolvedSubmission);
 }
 
 // description Intercepts the fetch request response, and returns the original response
@@ -16,20 +72,31 @@ async function handleInterceptResponse(response) {
     .then(parseLeetcodeFetchResponse)
     .catch(console.error);
 
+  // return the original response
   return response;
 }
 
 function handleInterceptError(error) {
-  console.log(error);
+  console.error(error);
 }
 
 // override fetch
 // source : https://blog.logrocket.com/intercepting-javascript-fetch-api-requests-responses/
 const { fetch: originalFetch } = window;
-window.fetch = async (...args) => {
-  const [resource, config] = args;
 
-  const interceptedResponse = await originalFetch(resource, config)
+window.fetch = async (...args) => {
+  const [fetchUrl, payload] = args;
+
+  if (Utils.isCreateSolutionRequest(fetchUrl)) {
+    solutionCode = JSON.parse(payload.body).typed_code;
+  }
+
+  // non-submission request - use orginal fetch
+  if (!Utils.isCheckSolutionRequest(fetchUrl)) {
+    return originalFetch(fetchUrl, payload);
+  }
+
+  const interceptedResponse = await originalFetch(fetchUrl, payload)
     .then(handleInterceptResponse)
     .catch(handleInterceptError);
 
